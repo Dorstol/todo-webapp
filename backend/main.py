479a0e06fa1e9a23ea2cf9e443c3de7318dc2ede
Dorstol <olehsofronov@gmail.com>
2025-01-14 11:02:39 +0200

    separate folders and update requirements

---
+++ backend/main.py
@@ -0,0 +1,28 @@
+from contextlib import asynccontextmanager
+
+
+from pydantic import BaseModel
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from models import init_db
+import requests as rq
+
+
+@asynccontextmanager
+async def lifespan(app_: FastAPI):
+    await init_db()
+    print("Bot is ready")
+    yield
+
+
+app = FastAPI(title="To Do App", lifespan=lifespan)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
